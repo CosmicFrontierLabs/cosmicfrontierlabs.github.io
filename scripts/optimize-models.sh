5ebb4e03e435947a3a7da8230e1deef330e83a4e
Alex Ledger <alex@alexledger.net>
2026-02-11 17:23:51 -0800

    Reoptimize models but keep the meshes

--- scripts/optimize-models.sh
+++ scripts/optimize-models.sh
@@ -6,5 +6,10 @@ for file in public/models/original/*.glb; do
   echo "Processing $name..."
   gltf-transform weld "$file" /tmp/welded.glb
   gltf-transform simplify /tmp/welded.glb /tmp/simplified.glb --ratio 0.1 --error 0.001
-  gltf-transform optimize /tmp/simplified.glb "public/models/$name" --compress meshopt
+
+  # Original
+  # gltf-transform optimize /tmp/simplified.glb "public/models/$name" --compress meshopt
+
+  # Keep meshes and mesh names
+  gltf-transform optimize /tmp/simplified.glb "public/models/$name" --compress meshopt --join false --flatten false
 done
