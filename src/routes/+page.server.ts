e1f2427238204757ffae1fd3b2e4d91d967cebac
Alex Ledger <alex@alexledger.net>
2026-03-04 12:18:56 -0800

    Update

--- src/routes/+page.server.ts
+++ src/routes/+page.server.ts
@@ -4,7 +4,7 @@ import yaml from "js-yaml";
 import type { CarouselItem } from "$lib/types";
 
 export function load() {
-  const carouselYamlPath = join(process.cwd(), "src/site-content/carouselData.yaml");
+  const carouselYamlPath = join(process.cwd(), "src/site-content/carousel.yaml");
   const carouselFile = readFileSync(carouselYamlPath, "utf-8");
   const carouselData = yaml.load(carouselFile) as CarouselItem[];
 
