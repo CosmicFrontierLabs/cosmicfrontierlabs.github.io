5e3f1f8e407bc01f8adeba43079e7128b2102c7f
Andrew Cantino <cantino@users.noreply.github.com>
2026-05-11 15:51:58 -0700

    Simple style

--- src/routes/blog/+page.svelte
+++ src/routes/blog/+page.svelte
@@ -8,9 +8,9 @@
 
 <section class="flow blog">
   <h1>Blog</h1>
-  <a class="button blog__rss-link" data-type="ghost" data-smaller href="/rss.xml" type="application/rss+xml">
-    Subscribe via RSS
-  </a>
+  <p class="blog__rss">
+    <a href="/rss.xml"> RSS</a>
+  </p>
 
   {#if data.posts.length > 0}
     <ul class="flow">
@@ -36,17 +36,13 @@
   .blog ul {
     padding-inline: 0;
     max-width: 800px;
-    margin-block-start: var(--space-m);
+    margin-block-start: var(--space-l);
     font-size: var(--size-step--1);
   }
 
-  .blog__rss-link {
-    --button-text: var(--color-primary-light);
-    --button-border: 1px solid var(--color-primary-dark);
-    --button-border-radius: var(--radius-xl);
-    --button-x-padding: 0.9em;
-    --button-y-padding: 0.35em;
-    margin-block-start: var(--space-2xs);
+  .blog__rss {
+    margin: 0;
+    font-size: var(--size-step--2);
   }
 
   .blog li {
